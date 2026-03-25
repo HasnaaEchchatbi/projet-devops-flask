b3ef76599312440c18fc45cf143db1ad3868aa61
HasnaaEchchatbi <echchatbihasnaa@gmail.com>
2026-03-25 00:33:36 +0000

    ajout

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-return "Hello hello?! Mon infrastructure DevOps fonctionne sur Kubernetes !"
+    # Il faut exactement 4 espaces (ou une tabulation) avant le "return"
+    return "Hello hello?! Mon infrastructure DevOps fonctionne sur Kubernetes !"
+
 if __name__ == '__main__':
+    # Il faut exactement 4 espaces avant "app.run"
     app.run(host='0.0.0.0', port=5000)
 
-
-    
