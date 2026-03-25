dea33279e9ec08235f6afe5d1e9a62ede3331c4d
HasnaaEchchatbi <echchatbihasnaa@gmail.com>
2026-03-25 13:49:40 +0000

    ajout css1

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def hello():
     <div style="text-align:center; padding:50px; font-family:sans-serif; background-color:#f0f4f8; height:100vh;">
         <div style="background:white; display:inline-block; padding:30px; border-radius:15px; box-shadow: 0 4px 6px rgba(0,0,0,0.1); border-top: 5px solid #007bff;">
             <h1 style="color:#007bff;">🚀 Projet Master DevOps</h1>
-            <p style="font-size:1.2em; color:#333;">Hello hello?! Mon infrastructure fonctionne sur <b>Kubernetes</b> !</p>
+            <p style="font-size:1.2em; color:#333;">Bonjour <b>Kubernetes</b> !</p>
             <span style="color:green;">✔ CI/CD Jenkins validée</span>
         </div>
     </div>
