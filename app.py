644fb4e9087c9364a4904863515ee4f6a3c553f3
HasnaaEchchatbi <echchatbihasnaa@gmail.com>
2026-03-25 00:37:47 +0000

    ajout css

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
+
+
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    # Il faut exactement 4 espaces (ou une tabulation) avant le "return"
-    return "Hello hello?! Mon infrastructure DevOps fonctionne sur Kubernetes !"
+    # IL FAUT ABSOLUMENT 4 ESPACES ICI AVANT LE MOT RETURN
+    return """
+    <div style="text-align:center; padding:50px; font-family:sans-serif; background-color:#f0f4f8; height:100vh;">
+        <div style="background:white; display:inline-block; padding:30px; border-radius:15px; box-shadow: 0 4px 6px rgba(0,0,0,0.1); border-top: 5px solid #007bff;">
+            <h1 style="color:#007bff;">🚀 Projet Master DevOps</h1>
+            <p style="font-size:1.2em; color:#333;">Hello hello?! Mon infrastructure fonctionne sur <b>Kubernetes</b> !</p>
+            <span style="color:green;">✔ CI/CD Jenkins validée</span>
+        </div>
+    </div>
+    """
 
 if __name__ == '__main__':
-    # Il faut exactement 4 espaces avant "app.run"
     app.run(host='0.0.0.0', port=5000)
-
